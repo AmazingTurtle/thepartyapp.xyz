2577fe33fd74a6d0463d627779a9e2b7b253d1b3
Turtle <turtle@turtledev.net>
2020-12-30 01:52:31 +0100

    remove offline support

--- next.config.js
+++ next.config.js
@@ -1,26 +1,9 @@
 // next.config.js
 const withSass = require('@zeit/next-sass');
-const withOffline = require('next-offline');
 module.exports = withSass(
     {
         postcssLoaderOptions: {
             plugins: [require('autoprefixer')({})]
-        },
-        ...withOffline({
-            workboxOpts: {
-                runtimeCaching: [
-                    {
-                        urlPattern: /^https?.*/,
-                        handler: 'NetworkFirst',
-                        options: {
-                            cacheName: 'offlineCache',
-                            expiration: {
-                                maxEntries: 200
-                            }
-                        }
-                    }
-                ]
-            }
-        })
+        }
     }
 );

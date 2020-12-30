8ce75d4c5cc858ccfcc4a26850b6bb37a0a935ed
Turtle <turtle@turtledev.net>
2020-12-30 01:56:21 +0100

    add next pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 // next.config.js
 const withSass = require('@zeit/next-sass');
+const withPWA = require('next-pwa');
 module.exports = withSass(
     {
         postcssLoaderOptions: {
             plugins: [require('autoprefixer')({})]
-        }
+        },
+        ...withPWA({
+            pwa: {
+                dest: 'public'
+            }
+        })
     }
 );

352afe4ad370bfb80919ff20cf3b414148d86853
turtle <linux@turtledev.net>
2021-01-12 17:48:26 +0100

    add GTAG and privacy stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import './_app.scss';
+import Head from 'next/head';
 
 export default function MyApp({Component, pageProps}) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-15TP6HJEBM"/>
+                <script dangerouslySetInnerHTML={{
+                    __html: 'window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag(\'js\', new Date());gtag(\'config\', \'G-15TP6HJEBM\');'
+                }}>
+
+                </script>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '../components/button';
 import Layout from '../components/layout';
 import {TitleText} from '../components/titleText';
+import Link from 'next/link';
 
 function IndexPage() {
     return (
@@ -12,6 +13,9 @@ function IndexPage() {
                 <Button className="button--opacity-low" click="/games/truthOrDare">Wahrheit oder Pflicht</Button>
                 <Button className="button--opacity-low" click="/games/bigKingsCup">Big King's Cup</Button>
                 <Button className="button--opacity-low" click="/games/magicConch">Magische Miesmuschel</Button>
+            </div>
+            <div className="privacy-link">
+                <Link href="/privacy"><a>Datenschutz</a></Link>
             </div>
             <div className="author">
                 eine App von <a href="https://github.com/AmazingTurtle" target="_blank">Felix Faust</a>
